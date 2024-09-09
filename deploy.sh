204584161e67f4126adafd58114fb1a450a1b8d6
xiaoweiqian <xiaoweiqian5@163.com>
2024-09-09 10:14:33 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# 自动部署脚本，部署到个人的github pages
+# 自动部署脚本，部署到个人的 github pages
+
+# 判断参数个数是否正确
+if [ $# -ne 1 ]; then
+    echo "用法：$0 <branch_name>"
+    exit 1
+fi
+
+BRANCH_NAME=$1
 
 # 构建
 echo '开始构建代码...'
@@ -14,7 +22,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 推到你仓库的 master 分支
+# 推到你仓库的指定分支
 echo '开始推送代码...'
-git push -f git@github.com:v-direwolf/v-direwolf.github.io.git master
+git checkout -b $BRANCH_NAME
+git push -f git@github.com:v-direwolf/v-direwolf.github.io.git $BRANCH_NAME
 echo '代码推送成功'
